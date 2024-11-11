a9080893924c77c7d4198ce365b50c7c92d42ae5
Michael Cruz <mcruz@aersale.com>
2024-11-11 16:56:45 -0500

    added recursion

--- main.go
+++ main.go
@@ -8,21 +8,21 @@ import (
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("no website provided")
-		os.Exit(1)
+		return
 	}
 	if len(os.Args) > 2 {
 		fmt.Println("too many arguments provided")
-		os.Exit(1)
+		return
 	}
 	rawBaseURL := os.Args[1]
 
 	fmt.Printf("starting crawl of: %s...\n", rawBaseURL)
 
-	htmlBody, err := getHTML(rawBaseURL)
-	if err != nil {
-		fmt.Println(err)
+	pages := make(map[string]int)
+
+	crawlPage(rawBaseURL, rawBaseURL, pages)
+
+	for normalizedURL, count := range pages {
+		fmt.Printf("%d - %s\n", count, normalizedURL)
 	}
-
-	fmt.Print(htmlBody)
-
 }
